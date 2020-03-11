b8f56a29c5cb7c5c38630b58d99d93753ccb2a3f
Joakim Kartveit <joakimkartveit@gmail.com>
2020-03-11 14:47:28 +0100

    Legger til flere commons i padm

---
+++ padm-common-networking/build.gradle.kts
@@ -0,0 +1,68 @@
+val coroutinesVersion = "1.3.3"
+val ktorVersion = "1.3.1"
+val logbackVersion = "1.2.3"
+val logstashEncoderVersion = "5.1"
+
+
+plugins {
+    id("java")
+    id("maven-publish")
+    id("org.sonarqube") version "2.7"
+}
+
+dependencies {
+    implementation(project(":padm-common-metrics"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+}
+
+subprojects {
+    properties["sonarHost"]?.let { host ->
+        sonarqube {
+            properties {
+                property("sonar.sourceEncoding", "UTF-8")
+                property("sonar.host.url", host)
+            }
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/padm-common")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            pom {
+                name.set("syfoam-common-networking")
+                description.set("Bibliotek for standar nettverksoppsett for dialogmelding domentet")
+                url.set("https://github.com/navikt/padm-common")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/padm-common.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/padm-common.git")
+                    url.set("https://github.com/navikt/padm-common")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
 rootProject.name = "padm-common"
 include ("padm-common-ws")
 include ("padm-common-mq")
+include ("padm-common-networking")
+include ("padm-common-metrics")
